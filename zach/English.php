d83c02f9cf450732200a6ba064fcd817aa8c4e47
Zachary <zacharyvachal@yahoo.com>
2018-01-30 13:27:45 -0800

    cleanup

--- zach/English.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head>
-    <title>Study Stack</title>
-</head>
-<body>
-<iframe class="studyStackFlashcardIframe" src="https://www.studystack.com/inewflashcard-592412" width="850" height="440" frameborder="0" scrolling="no" style="overflow:hidden"></iframe>
-</body>
-
-</html>
